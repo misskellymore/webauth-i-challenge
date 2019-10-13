1cd5338b87027ad8812b75cd4d709885d8df09a8
Kelly Moreira <moreira.kd@outlook.com>
2019-10-13 19:27:29 -0400

    post post get

--- auth/user-model.js
+++ auth/user-model.js
@@ -3,13 +3,31 @@ const db = require('../db/dbConfig.js');
 
 function insert(user) {
 
-    return (
-        db('users').insert(user, 'id')
-                .then(([id]) => id) 
-    )
+    return  db('users').insert(user, 'id')
+           .then(([id]) => id) 
+    
 
 };
 
+
+function findBy(where) {
+    return db('users').where(where);
+};
+
+
+function findByUserName(username) {
+    return findBy({username}).first();
+};
+
+function find() {
+    return db('users');
+}
+
 module.exports = {
+
+    insert,
+    findBy,
+    findByUserName,
+    find
 
 };
--- auth/user-router.js
+++ auth/user-router.js
@@ -3,17 +3,54 @@ const model = require('./user-model.js');
 const bcrypt = require('bcryptjs');
 
 
+// get users
+route.get('/', (req, res) => {
+    model.find()
+    .then(users => {
+        res.json(users)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({message: 'err getting users'})
+    })
+
+})
+
+
+// sign up user
 route.post('/register', (req, res) => {
     
-    const {username, password} = req.headers;
+    const {username, password} = req.body;
 
     model.insert({username, password: bcrypt.hashSync(password, 2)})
           .then(id => {
-              res.status(201).json(id);
+              res.status(201).json({message: 'user registered', id});
           })
           .catch(err => {
               console.log(err);
               res.status(500).json({err: 'err registering user'});
+          });
+});
+
+
+
+// user login
+
+route.post('/login', (req, res) => {
+    
+    const {username, password} = req.body;
+
+    model.findByUserName(username)
+          .then(user => {
+              if (user && bcrypt.compareSync(password, user.password)) {
+                  res.status(200).json({message: 'you are logged in'});
+              } else {
+                  res.status(401).json({err : 'invalid username or password'});
+              }
+          })
+          .catch(err => {
+              console.log(err);
+              res.status(500).json({err: 'err logging in user'});
           });
 });
 
